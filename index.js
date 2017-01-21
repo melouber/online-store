a8e508892a16ab00ce9aa294496828554ce5181c
Mateusz Lewko <mateusz.lewko@gmail.com>
2017-01-21 14:56:44 +0100

    Add to cart button, search text field

--- index.js
+++ index.js
@@ -29,26 +29,42 @@ var users_db = {
 }
 
 var products_db = [
-    { name: 'Bloody Mary', price: 3, description: 'Dobra woda' },
-    { name: 'Martini', price: 5, description: 'Zla woda' },
-    { name: 'Scotch', price: 10, description: 'Ok woda' }
+    { id: 1, name: 'Bloody Mary', price: 3, description: 'Dobra woda' },
+    { id: 2, name: 'Martini', price: 5, description: 'Zla woda' },
+    { id: 3, name: 'Scotch', price: 10, description: 'Ok woda' }
 ]
 
 // -- Views -- //
 
 app.get('/', (req, res) => {
-    res.render('products_out', { products: products_db });
+    // req.session.lastSite = '/products_out';
+    res.redirect(req.cookies.user ? '/products_in' : '/products_out');
     res.end();
 });
 
 app.get('/products_in', auth, (req, res) => {
-    res.render('products_in', { products: products_db });
+    res.render('products_in', { products: products_db, user_name: req.cookies.user });
+    res.end();
+});
+
+app.get('/products_out', (req, res) => {
+    if (req.cookies.user)
+        res.redirect('/products_in');
+    else 
+        res.render('products_out', { products: products_db });
     res.end();
 });
 
 app.get('/login', (req, res) => {
     res.render('login');
 })
+
+products_db.forEach(product => {
+    app.post('/add_to_cart' + product.id, auth, (req, res) => {
+        console.log(product.name + ' added to cart');
+        res.redirect('/products_in');
+    });
+});
 
 app.post('/login', (req, res) => {
     console.log('login : ' + req.body.login);
@@ -58,8 +74,10 @@ app.post('/login', (req, res) => {
         res.cookie('user', req.body.login);
 		res.redirect('/products_in');
     }
-    else
-        res.send("Login and pass don't match");
+    else {
+        // console.log('last site ' + req.session.lastSite);
+        res.render('login', { locals : { message : "Login and pass don't match" } });
+    }
 })
 
 function auth(req, res, next) {
